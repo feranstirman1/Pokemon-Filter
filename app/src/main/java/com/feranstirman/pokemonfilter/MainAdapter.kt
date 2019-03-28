d0316d779bd24204d57b58f2f0b9063efd195d94
feranstirman1 <00024117@uca.edu.sv>
2019-03-28 17:19:26 -0600

    nombres individuales se asignan a cada pokemon correctamente

--- app/src/main/java/com/feranstirman/pokemonfilter/MainAdapter.kt
+++ app/src/main/java/com/feranstirman/pokemonfilter/MainAdapter.kt
@@ -4,10 +4,14 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.pokemon_row.view.*
 
 class MainAdapter: RecyclerView.Adapter<CustomViewHolder>(){
+
+    val pokemonNames = listOf<String>("charmander","squirtle","bulbasaur")
+
     override fun getItemCount(): Int {
-        return 10
+        return pokemonNames.size
     }
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomViewHolder {
@@ -17,6 +21,8 @@ class MainAdapter: RecyclerView.Adapter<CustomViewHolder>(){
     }
 
     override fun onBindViewHolder(p0: CustomViewHolder, p1: Int) {
+        val pokemonName = pokemonNames.get(p1)
+        p0.itemView.tv_pokemon_name.text = pokemonName
 
     }
 
